34681652268e462cc90c30f501e1e8a1915a52c1
JFelipeAB <joao-alves-borges@hotmail.com>
2021-11-19 21:28:09 -0300

    Script Python 2

--- Docker/imagens.py
+++ Docker/imagens.py
@@ -1,24 +1,6 @@
 from docker import DockerClient
 import docker
 
-
-
- 
-    
-
-def criar(image, tag):
-    docker_client = DockerClient(base_url='tcp://localhost:2375', version='auto')
-    docker_client.images.pull(image,tag)
-
-def apagar(id):
-    docker_client = DockerClient(base_url='tcp://localhost:2375', version='auto')
-    docker_client.images.get(id).remove()
-    
-def apagarTodos():
-    docker_client = DockerClient(base_url='tcp://localhost:2375', version='auto')
-    for image in listar():
-        docker_client.images.remove(image)
-#resp = ''   
 def menu():
     print('''
         MENU:
@@ -27,33 +9,37 @@ def menu():
         [2] - Baixar uma Imagem
         [3] - Apagar Imagem
         [4] - Apagar todas as imagens Imagens
+        [5] - Quit
         '''
     )
     return int(input('Escolha uma opção: '))
     
 resp = menu()
+while (resp != 5):
+    if(resp == 1):
+        docker_client = DockerClient(base_url='tcp://localhost:2375', version='auto')
+        list = docker_client.images.list(all=True) 
+        print (list)
+    if(resp == 2):
+        repository = str(input('Digite o repositório da imagem: '))
+        name = str(input('Digite o nome da imagem: '))
 
-if(resp == 1):
-    docker_client = DockerClient(base_url='tcp://localhost:2375', version='auto')
-    list = docker_client.images.list(all=True) 
-    print (list)
-if(resp == 2):
-    tag = str(input('Digite o nome da imagem: '))
-    docker_client = DockerClient(base_url='tcp://localhost:2375', version='auto')
-    docker_client.images.pull(tag)
-    print ('Imagem criada com sucesso! ')
+        docker_client = DockerClient(base_url='tcp://localhost:2375', version='auto')
+        docker_client.images.pull(repository, name)
+        print ('Imagem criada com sucesso! ')
 
-if(resp == 3):
-    name = str(input('Digite o nome da imagem que deseja excluir: '))
-    docker_client = DockerClient(base_url='tcp://localhost:2375', version='auto')
-    docker_client.images.remove(name)
-    print ('Imagem excluída com sucesso! ')
+    if(resp == 3):
+        name = str(input('Digite o nome da imagem que deseja excluir: '))
+        docker_client = DockerClient(base_url='tcp://localhost:2375', version='auto')
+        docker_client.images.remove(name)
+        print ('Imagem excluída com sucesso! ')
 
 
-if(resp == 4):    
-    docker_client = DockerClient(base_url='tcp://localhost:2375', version='auto')
-    lista = docker_client.images.list(all=True)  
-    for image in lista:
-        docker_client.images.remove(image)
-
+    if(resp == 4):    
+        docker_client = DockerClient(base_url='tcp://localhost:2375', version='auto')
+        r = str ('Deseja realmente excluir todas as imagens? ')
+        lista = docker_client.images.list(all=True)  
+        for image in lista:
+            docker_client.images.remove(image)
+    resp = menu() 
 print ('Programa Finalizado')
